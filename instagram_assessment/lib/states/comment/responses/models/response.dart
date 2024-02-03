0d18015b882acdfa116e060454faca0104f4614b
haukutekontum <haukutekontum@gmail.com>
2024-02-04 00:55:21 +0700

    done response model

--- instagram_assessment/lib/states/comment/responses/models/response.dart
+++ instagram_assessment/lib/states/comment/responses/models/response.dart
@@ -1,27 +1,30 @@
+import 'dart:collection';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:instagram_assessment/states/auth/typedef/user_id.dart';
-import 'package:instagram_assessment/states/comment/likes_comment/models/liked_comment.dart';
+import 'package:instagram_assessment/states/constants/firebase_field_name.dart';
 import 'package:instagram_assessment/states/post/constants/post_key.dart';
 
 @immutable
-class Response {
+class Response extends MapView<dynamic, dynamic> {
   final String comment;
   final UserId userId;
   final DateTime createAt;
-  // final Iterable<LikedComment> likes;
 
-  Response({required Map<dynamic, dynamic> json})
-      : comment = json[PostKey.comment],
-        userId = json[PostKey.userId],
-        createAt =
-            (json[PostKey.createAt] as Timestamp?)?.toDate() ?? DateTime.now();
-  //       likes = _parseLikes(json[PostKey.likes]);
+  Response(
+      {required this.comment, required this.userId, required this.createAt})
+      : super({
+          FirebaseFieldName.comment: comment,
+          FirebaseFieldName.userId: userId,
+          FirebaseFieldName.createAt: createAt,
+        });
 
-  // static Iterable<LikedComment> _parseLikes(dynamic json) {
-  //   if (json is List) {
-  //     return json.map((like) => LikedComment(json: like)).toList();
-  //   }
-  //   return [];
-  // }
+  Response.fromJson({required Map<String, dynamic> json})
+      : this(
+          comment: json[PostKey.comment],
+          userId: json[PostKey.userId],
+          createAt: (json[PostKey.createAt] as Timestamp?)?.toDate() ??
+              DateTime.now(),
+        );
 }
---
+++ instagram_assessment/lib/states/comment/responses/models/response_payload.dart
@@ -0,0 +1,19 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:instagram_assessment/states/auth/typedef/user_id.dart';
+import 'package:instagram_assessment/states/constants/firebase_field_name.dart';
+
+@immutable
+class ResponsePayLoad extends MapView<String, dynamic>{
+  ResponsePayLoad({
+     required String comment,
+  required UserId userId
+  }):super({
+    FirebaseFieldName.comment : comment,
+    FirebaseFieldName.userId : userId,
+    FirebaseFieldName.createAt: FieldValue.serverTimestamp(),
+  });
+  
+}
